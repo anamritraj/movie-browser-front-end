67941fb5098911f005c022775452fa132755cd63
anamritraj <anandamritraj904@gmail.com>
2018-09-01 15:30:01 +0530

    Add movies component.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 import './App.css';
 import Header from './components/header/header';
+import Movies from './containers/movies/Movies';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header></Header>
-        <p className="">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Movies></Movies>
       </div>
     );
   }
---
+++ src/containers/movies/Movies.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Movies extends Component {
+    render() {
+        return (
+            <div>
+                Movies
+            </div>
+        )
+    }
+}
